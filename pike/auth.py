b8accedc25f72d4dad43e8649c1b72606ed9a943
Masen Furer <mfurer@isilon.com>
2017-06-30 00:15:01 +0000

    add documentation to pike.auth module
    also fixes some PEP8 issues and updates the copyright notice

--- pike/auth.py
+++ pike/auth.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2016, Dell Technologies
+# Copyright (c) 2017, Dell Technologies
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -37,7 +37,8 @@
 """
 Authentication Plugins for Pike
 
-This module contains wrappers around external authentication mechanisms and APIs.
+This module contains wrappers around external authentication mechanisms and
+APIs.
 """
 
 
@@ -53,6 +54,16 @@ except ImportError:
 
 
 def split_credentials(creds):
+    """
+    Split samba-style credential string
+
+    @type creds: string
+    @param creds: credential in the format DOMAIN\\user%passwd (domain
+        may be ommitted)
+    @rtype: tuple of (string, string, string)
+    @return: (domain, user, password). If domain is not specified it
+        will be returned as the string "None"
+    """
     user, password = creds.split('%')
     if '\\' in user:
         domain, user = user.split('\\')
@@ -61,8 +72,66 @@ def split_credentials(creds):
     return (domain, user, password)
 
 
-class KerberosProvider(object):
+class GenericProvider(object):
+    """
+    Abstract base class for documentation purposes. Authentication providers
+    should inherit from this class.
+    """
+    def step(self, sec_buf):
+        """
+        perform one authentication step
+
+        @type sec_buf: array.array
+        @param sec_buf: opaque security buffer
+
+            sec_buf is typically the C{security_buffer} field of a
+            L{NegotiateResponse} or L{SessionSetupResponse}
+        @rtype: tuple of (array.array, array.array)
+        @return: (opaque security buffer, session key) either of which may be
+            None indicating that the field is not applicable to this step in
+            the authentication process
+        """
+        raise NotImplementedError
+
+    def username(self):
+        """
+        @rtype: string
+        @return: the username that has authenticated against the provider.
+
+            Note: this function may only be applicable after authentication
+            steps have completed
+        """
+        raise NotImplementedError
+
+
+class KerberosProvider(GenericProvider):
+    """
+    Leverage pykerb library to exchange multiple rounds of gssapi as necessary
+    in order to perform Kerberos authentication.
+
+    Depending on the gss mechanisms available in the kerberos library that
+    pykerb was built against, this provider may also support NTLM. The only
+    known kerberos library supporting NTLM is from likewise.
+
+    This provider requires that pike has been built with pykerb/kerberos C
+    extension.
+    """
     def __init__(self, conn, creds=None):
+        """
+        Initialize the KerberosProvider instance
+
+        @type conn: L{Connection}
+        @param conn: the L{Connection} that the authentication is being
+            performed against
+        @type creds: string or None
+        @param creds: if None, an externally acquired kerberos ticket will be
+            used for authentication.
+
+            Otherwise a string credential in the format DOMAIN\\user%passwd
+            will trigger NTLM to be used (if supported by library)
+
+            creds defaults to None
+        """
         if creds:
             domain, user, password = split_credentials(creds)
             (self.result,
@@ -89,15 +158,30 @@ class KerberosProvider(object):
         else:
             kerberos.authGSSClientSessionKey(self.context)
             return (None,
-                    array.array('B',
+                    array.array(
+                        'B',
                         kerberos.authGSSClientResponse(self.context)[:16]))
 
     def username(self):
         return kerberos.authGSSClientUserName(self.context)
 
 
-class NtlmProvider(object):
+class NtlmProvider(GenericProvider):
+    """
+    Leverage L{ntlm} module to perform multiple rounds of NTLM authentication
+
+    Both NTLMv1 and NTLMv2 are available
+    """
     def __init__(self, conn, creds):
+        """
+        Initialize the NtlmProvider instance
+
+        @type conn: L{Connection}
+        @param conn: the L{Connection} that the authentication is being
+            performed against
+        @type creds: string
+        @param creds: credential in the format DOMAIN\\user%passwd
+        """
         self.authenticator = ntlm.NtlmAuthenticator(*split_credentials(creds))
 
     def step(self, sec_buf):
@@ -105,7 +189,9 @@ class NtlmProvider(object):
             return (self.authenticator.negotiate(), None)
         elif self.authenticator.challenge_message is None:
             self.authenticator.authenticate(sec_buf)
-        return (self.authenticator.authenticate_buffer, self.authenticator.exported_session_key)
+        return (self.authenticator.authenticate_buffer,
+                self.authenticator.exported_session_key)
 
     def username(self):
-        return '{0}\{1}'.format(self.authenticator.domain, self.authenticator.username)
+        return '{0}\{1}'.format(self.authenticator.domain,
+                                self.authenticator.username)
