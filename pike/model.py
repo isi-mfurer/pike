3488e82b068fa03c765037a5ed52ed3849a846d8
Masen Furer <mfurer@isilon.com>
2021-02-11 23:47:31 -0800

    pike.model: Tree.pathlike returns a "root"
    represent the share as a root path rather than a current directory "."
    path
    
    update pike.path to trim off the leading "\\" when passing the path
    to create

--- pike/model.py
+++ pike/model.py
@@ -2160,8 +2160,9 @@ class Tree(object):
     def pathlike(self):
         from . import path
         return path.PikePath(
-            channel=self.session.first_channel(),
-            tree=self,
+            self.session.first_channel(),
+            self,
+            "/",
         )
 
 
